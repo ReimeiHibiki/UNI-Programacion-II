f798c9bca1527cbb957905ee59e9b059d14c71dd
Reinenechi <113166123+ReimeiHibiki@users.noreply.github.com>
2022-09-10 22:33:17 -0500

    Update Programa A1b2 - Calificaciones usando Vectores v2.c
    Formatted automatically using Prettier (Visual Studio Code Extension).

--- Vectores/Programa A1b2 - Calificaciones usando Vectores v2.c
+++ Vectores/Programa A1b2 - Calificaciones usando Vectores v2.c
@@ -1,23 +1,23 @@
-//MODIFICAR EL CODIGO PARA QUE EN BASE A UNA CALIFICACION FINAL DE UN ALUMNO
-//DETERMINE SI ESTA APROBADO O REPROBADO
-//VERSION NUMERO 2
-#include<stdio.h>
-#include<conio.h>
-#define n 5 //Numero de alumnos a evaluar
+// MODIFICAR EL CODIGO PARA QUE EN BASE A UNA CALIFICACION FINAL DE UN ALUMNO
+// DETERMINE SI ESTA APROBADO O REPROBADO
+// VERSION NUMERO 2
+#include <stdio.h>
+#include <conio.h>
+#define n 5 // Numero de alumnos a evaluar
 
-int main (void)
+int main(void)
 {
-  int calificaciones[n];	//Este arreglo es un VECTOR
-  int i;			//Contador
+  int calificaciones[n]; // Este arreglo es un VECTOR
+  int i;                 // Contador
 
-  for (i = 0; i < n; i++)	//i=i+1;
-    {
-      printf ("\nProporcione la calificacion del alumno %d: \n", i);
-      scanf ("%d", &calificaciones[i]);
-        if (calificaciones[i] >= 70)
-    printf ("El alumno ha APROBADO.\n");
-        else
-    printf ("El alumno ha REPROBADO.\n");
-    }
-  getch ();
+  for (i = 0; i < n; i++) // i=i+1;
+  {
+    printf("\nProporcione la calificacion del alumno %d: \n", i);
+    scanf("%d", &calificaciones[i]);
+    if (calificaciones[i] >= 70)
+      printf("El alumno ha APROBADO.\n");
+    else
+      printf("El alumno ha REPROBADO.\n");
+  }
+  getch();
 }
