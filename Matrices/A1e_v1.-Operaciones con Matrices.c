83cb493c2c24fe24afde05292c093edb15e8f9f0
Reinenechi <113166123+ReimeiHibiki@users.noreply.github.com>
2022-09-10 22:42:59 -0500

    Update A1e_v1.-Operaciones con Matrices.c
    -Removed <conio.h> library
    +Added <stdlib.h> library for system("pause") function
    
    Formatted using Prettier (Visual Studio Code Extension).

--- Matrices/A1e_v1.-Operaciones con Matrices.c
+++ Matrices/A1e_v1.-Operaciones con Matrices.c
@@ -1,96 +1,93 @@
-//OPERACIONES BASICAS CON MATRICES: LLENAR, IMPRIMIR, BUSCAR, MODIFICAR Y ELIMINAR
-#include<stdio.h>
-#include<conio.h>
+// OPERACIONES BASICAS CON MATRICES: LLENAR, IMPRIMIR, BUSCAR, MODIFICAR Y ELIMINAR
+#include <stdio.h>
+#include <stdlib.h>
 
-int
-main (void)
+int main(void)
 {
   int buscar_valor, modificar_valor;
   int valores[3][3], f, c;
 
-  for (f = 0; f < 3; f++)	//LLENADO
+  for (f = 0; f < 3; f++) // LLENADO
     for (c = 0; c < 3; c++)
-      {
-	printf ("Da un valor entero: ");
-	scanf ("%d", &valores[f][c]);
-      }
-
-  printf ("\n\n");
-  for (f = 0; f < 3; f++)	//IMPRIMIR
     {
-      for (c = 0; c < 3; c++)
-	{
-	  printf ("\t%d", valores[f][c]);
-	}
-      printf ("\n\n");
+      printf("Da un valor entero: ");
+      scanf("%d", &valores[f][c]);
     }
 
-  printf ("\nDa el valor a buscar: ");
-  scanf ("%d", &buscar_valor);
-
-  for (f = 0; f < 3; f++)	//BUSCAR
+  printf("\n\n");
+  for (f = 0; f < 3; f++) // IMPRIMIR
+  {
+    for (c = 0; c < 3; c++)
     {
-      for (c = 0; c < 3; c++)
-	{
-	  if (buscar_valor == valores[f][c])
-	    printf ("Se encontro el valor %d en la posicion [%d,%d]!\n",
-		    valores[f][c], f, c);
-	  else
-	    printf ("NO se encontro el valor!\n");
-	}
+      printf("\t%d", valores[f][c]);
     }
+    printf("\n\n");
+  }
+
+  printf("\nDa el valor a buscar: ");
+  scanf("%d", &buscar_valor);
+
+  for (f = 0; f < 3; f++) // BUSCAR
+  {
+    for (c = 0; c < 3; c++)
+    {
+      if (buscar_valor == valores[f][c])
+        printf("Se encontro el valor %d en la posicion [%d,%d]!\n",
+               valores[f][c], f, c);
+      else
+        printf("NO se encontro el valor!\n");
+    }
+  }
 
   if (f == 0 && c == 0)
-    printf ("\nNO se encontro el valor!\n");
+    printf("\nNO se encontro el valor!\n");
 
-  printf ("\nDa el valor a modificar: \n");
-  scanf ("%d", &buscar_valor);
+  printf("\nDa el valor a modificar: \n");
+  scanf("%d", &buscar_valor);
 
-  printf ("\nInserte el nuevo valor: \n");
-  scanf ("%d", &modificar_valor);
+  printf("\nInserte el nuevo valor: \n");
+  scanf("%d", &modificar_valor);
 
-  for (f = 0; f < 3; f++)	//MODIFICAR
+  for (f = 0; f < 3; f++) // MODIFICAR
+  {
+    for (c = 0; c < 3; c++)
     {
-      for (c = 0; c < 3; c++)
-	{
 
-	  if (buscar_valor == valores[f][c])
-	    valores[f][c] = modificar_valor;
-	}
+      if (buscar_valor == valores[f][c])
+        valores[f][c] = modificar_valor;
     }
+  }
 
-  for (f = 0; f < 3; f++)	//IMPRIMIR (Para confirmar cambio en la matriz)
+  for (f = 0; f < 3; f++) // IMPRIMIR (Para confirmar cambio en la matriz)
+  {
+    for (c = 0; c < 3; c++)
     {
-      for (c = 0; c < 3; c++)
-	{
-	  printf ("\t%d", valores[f][c]);
-	}
-      printf ("\n\n");
+      printf("\t%d", valores[f][c]);
     }
+    printf("\n\n");
+  }
 
-  printf ("\nDa el valor a eliminar: \n");
-  scanf ("%d", &buscar_valor);
+  printf("\nDa el valor a eliminar: \n");
+  scanf("%d", &buscar_valor);
 
-  for (f = 0; f < 3; f++)	//ELIMINAR
+  for (f = 0; f < 3; f++) // ELIMINAR
+  {
+    for (c = 0; c < 3; c++)
     {
-      for (c = 0; c < 3; c++)
-	{
 
-	  if (buscar_valor == valores[f][c])
-	    valores[f][c] = 0;
-	}
+      if (buscar_valor == valores[f][c])
+        valores[f][c] = 0;
     }
+  }
 
-  for (f = 0; f < 3; f++)	//IMPRIMIR (Para confirmar cambio en la matriz)
+  for (f = 0; f < 3; f++) // IMPRIMIR (Para confirmar cambio en la matriz)
+  {
+    for (c = 0; c < 3; c++)
     {
-      for (c = 0; c < 3; c++)
-	{
-	  printf ("\t%d", valores[f][c]);
-	}
-      printf ("\n\n");
+      printf("\t%d", valores[f][c]);
     }
+    printf("\n\n");
+  }
 
-
-
-  getch ();
+  system("pause");
 }
